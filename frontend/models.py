6fe593a6e9dcac5e22a307072bcd3d00d4667581
Kauê Alcântara Leivas <67640809+yyggrasil@users.noreply.github.com>
2025-09-30 10:08:57 -0300

    correção no modelo do banco de dados

--- frontend/models.py
+++ frontend/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 # Create your models here.
 class flights(models.Model):
-    flight_iata = models.CharField(max_length=3, null=True, blank=True)
+    flight_iata = models.CharField(max_length=10, null=True, blank=True)
     flight_icao = models.CharField(max_length=10, null=True, blank=True)
     flight_number = models.CharField(max_length=10, null=True, blank=True)
     airline_name = models.CharField(max_length=100, null=True, blank=True)
     airline_iata = models.CharField(max_length=3, null=True, blank=True)
     airline_icao = models.CharField(max_length=10, null=True, blank=True)
     departure_airport = models.CharField(max_length=100, null=True, blank=True)
-    departure_iata = models.CharField(max_length=3, null=True, blank=True)
+    departure_iata = models.CharField(max_length=10, null=True, blank=True)
     departure_scheduled = models.DateTimeField(null=True, blank=True)
     arrival_airport = models.CharField(max_length=100, null=True, blank=True)
-    arrival_iata = models.CharField(max_length=3, null=True, blank=True)
+    arrival_iata = models.CharField(max_length=10, null=True, blank=True)
     arrival_scheduled = models.DateTimeField(null=True, blank=True)
     status = models.CharField(max_length=50, null=True, blank=True)
     preco = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
